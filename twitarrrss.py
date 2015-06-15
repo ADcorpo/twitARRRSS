93770cb1f4dadb9c9aca8ced177eb92b143c5ab8
Cheapseth <cheapseth@electric-dragons.org>
2015-06-15 12:42:30 +0200

    Made format_tweet() more generic

--- twitarrrss.py
+++ twitarrrss.py
@@ -14,23 +14,16 @@ api = Twitter(auth=OAuth(credentials['token'],
 app = Flask(__name__)
 
 def format_tweet(tweet):
-    created_at = datetime.strptime(tweet['created_at'].replace('+0000','UTC'), '%a %b %d %H:%M:%S %Z %Y')
-    title = "@" + username + ': ' + tweet['text']
+    tweet['created_at'] = datetime.strptime(tweet['created_at'].replace('+0000','UTC'), '%a %b %d %H:%M:%S %Z %Y')
+    tweet['title'] = "@" + tweet['user']['name'] + ': ' + tweet['text']
 
     if 'media' in tweet['entities']:
-        content += "<img src='{}'>".format(tweet['entities']['media'][0]['media_url'])
+        tweet['text'] += "<img src='{}'>".format(tweet['entities']['media'][0]['media_url'])
 
-    content += "<a href='{}'>link to the tweet…</a>"
-    content.format("https://twitter.com/" + username + "/status/" + tweet['id_str'])
+    tweet['text'] += "<a href='{}'>link to the tweet…</a>"
+    tweet['text'] = tweet['text'].format("https://twitter.com/" + tweet['user']['name'] + "/status/" + tweet['id_str'])
 
-    tweet_data = \
-                 {
-                     'created_at': created_at,
-                     'title': title,
-                     'content': content
-                 }
-
-    return tweet_data
+    return tweet
     
 
 @app.route('/timeline/<username>')
@@ -38,20 +31,20 @@ def get_user_timeline(username):
     feed = AtomFeed(username + "'s timeline",
                     feed_url = request.url,
                     url = request.url_root)
-
+    
     #Getting the latest tweets, formatting data
     timeline = api.statuses.user_timeline(screen_name=username,
                                           count=200,
                                           exclude_replies=True)
 
     for tweet in timeline:
-        tweet_data = format_tweet(tweet)
+        tweet = format_tweet(tweet)
         
-        feed.add(tweet_data['title'],
-                 tweet_data['content'],
+        feed.add(tweet['title'],
+                 tweet['text'],
                  content_type='html',
-                 author=username,
-                 updated=tweet_data['created_at'],
+                 author=tweet['user']['name'],
+                 updated=tweet['created_at'],
                  id=tweet['id'])
 
     return feed.get_response()
